6a9f85993688f4a294885f3a7471a6709c022784
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-02-27 09:20:06 +0100

    ex-complete

--- java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -2,7 +2,17 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
-        System.out.println();
-    }
+        // nome, cognome, colore preferito, data di nascita
+        String nome = "Giovanni";
+        String cognome = "Lumini";
+        String colorePreferito = "Rosso";
+        int dataNascitaGiorno = 15;
+        int dataNascitaMese = 1;
+        int dataNascitaAnno = 1997;
 
+        int SommaNumDataNascita = dataNascitaGiorno + dataNascitaMese + dataNascitaAnno;
+        /* System.out.println(SommaNumDataNascita); */
+
+        System.out.println(nome + "-" + cognome + "-" + colorePreferito + "-" + SommaNumDataNascita);
+    }
 }
