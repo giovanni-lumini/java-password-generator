0398713da49ddc4549c323ea93b34979bd166c47
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-02-27 09:52:45 +0100

    value of variables from input

--- java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -1,18 +1,43 @@
 package org.lessons.java.security;
 
+//importo lo scanner per leggere gli input dalla console
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        // nome, cognome, colore preferito, data di nascita
-        String nome = "Giovanni";
-        String cognome = "Lumini";
-        String colorePreferito = "Rosso";
-        int dataNascitaGiorno = 15;
-        int dataNascitaMese = 1;
-        int dataNascitaAnno = 1997;
 
+        // scanner per leggere gli input dalla console
+        Scanner input = new Scanner(System.in);
+
+        // nome
+        String nome;
+        nome = input.nextLine();
+
+        // cognome
+        String cognome;
+        cognome = input.nextLine();
+
+        // colore preferito
+        String colorePreferito;
+        colorePreferito = input.nextLine();
+
+        // data di nascita giorno
+        int dataNascitaGiorno;
+        dataNascitaGiorno = input.nextInt();
+
+        // data di nascita mese
+        int dataNascitaMese;
+        dataNascitaMese = input.nextInt();
+
+        // data di nascita anno
+        int dataNascitaAnno;
+        dataNascitaAnno = input.nextInt();
+
+        // somma numeri data di nascita
         int SommaNumDataNascita = dataNascitaGiorno + dataNascitaMese + dataNascitaAnno;
-        /* System.out.println(SommaNumDataNascita); */
+        System.out.println(SommaNumDataNascita);
 
+        // password generata
         System.out.println(nome + "-" + cognome + "-" + colorePreferito + "-" + SommaNumDataNascita);
     }
 }
